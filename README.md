67128b8f8fd96ffd3b28a3e013508bff39bcd256
Rajit Manohar <rajit.manohar@yale.edu>
2023-02-18 12:36:41 -0500

    added lib2act script

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Utility Scripts
 
 This repository contains a number of useful scripts for working with the ACT flow.
+
+
+## lib2act.py
+
+This is a quick-and-dirty script that does a very simple line-based
+string parsing of a Liberty file to find all the cell names and pin
+names. These are then emitted as an ACT file that contains all the
+cell definitions specified using the ACT black-box syntax.
+
+If your design instantiates these cells, then ACT will assume they
+are specified in some external format (e.g. provided by a 
+cell library vendor), and treats them as black boxes in the design.
+
+To use, run `lib2act.py < file.lib > out.act`
---
+++ lib2act.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2022 Rajit Manohar
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+# Generate black-box ACT from a .lib file.
+#
+# Usage: lib2act.py < file.lib > out.act
+#
+# This file is a hack. It assumes that the .lib file uses newlines in
+# a nice way...
+#
+#
+
+import sys
+import re
+
+#
+# Flag to indicate a new cell was found
+#
+new_cell = False
+
+cell_expr = re.compile("cell\s*\(\s*([a-zA-Z_0-9]+)\s*\)")
+pin_expr = re.compile("pin\s*\(\s*([a-zA-Z_0-9]+)\s*\)")
+dir_expr = re.compile("direction\s*:\s*(input|output|bidir)\s*;")
+
+def print_cell(nm, inl, outl, bidirl):
+    print('export defcell', nm, '(',end='')
+    prev = False
+    if len(inl) > 0:
+        print('bool?', ", ".join(inl), end='')
+        prev = True
+    if len(outl) > 0:
+        if prev:
+            print("; ", end='')
+        print('bool!', ", ".join(outl), end='')
+        prev = True
+    if len(bidirl) > 0:
+        if prev:
+            print("; ", end='')
+        print('bool', ", ".join(bidirl), end='')
+    print(");")
+    
+
+for line in sys.stdin.readlines():
+    line.rstrip()
+    m = cell_expr.search(line)
+    if m != None:
+        if new_cell:
+            print_cell(cell_name, cell_in, cell_out, cell_bidir)
+        new_cell = True
+        cell_name = m.group(1)
+        cell_in = []
+        cell_out = []
+        cell_bidir = []
+        found_pin = False
+    else:
+        m = pin_expr.search(line)
+        if m != None and new_cell:
+            found_pin = True
+            prev_pin = m.group(1)
+        else:
+            m = dir_expr.search(line)
+            if m != None and found_pin:
+                if m.group(1) == "input":
+                    if not prev_pin in cell_in: cell_in.append(prev_pin)
+                    found_pin = False
+                elif m.group(1) == "output":
+                    if not prev_pin in cell_out: cell_out.append(prev_pin)
+                    found_pin = False
+                else:
+                    cell_bidir.append(prev_pin)
+                    found_pin = False
+
+if new_cell:
+    print_cell(cell_name, cell_in, cell_out, cell_bidir)
